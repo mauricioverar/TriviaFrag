c8f65c8fe2b8426ee90d7117250aa18c303d5d0c
Mauricio <stb539@gmail.com>
2024-04-27 19:21:37 -0400

    retroceder

--- app/src/main/java/com/example/triviafrag/MainActivity.java
+++ app/src/main/java/com/example/triviafrag/MainActivity.java
@@ -8,8 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.triviafrag.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,7 +20,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 }
