6f15adb5e0570459a4d202a29cb7d7cd9523c76e
Mauricio <stb539@gmail.com>
2024-05-06 19:11:19 -0400

    transaction

--- app/src/main/java/com/example/triviafrag/Inicio.java
+++ app/src/main/java/com/example/triviafrag/Inicio.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.SurfaceControl;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.triviafrag.databinding.FragmentInicioBinding;
 
 public class Inicio extends Fragment {
 
@@ -20,6 +25,8 @@ public class Inicio extends Fragment {
     //View vista;
     //Button button;
 
+
+
     public Inicio() {
         // Required empty public constructor
     }
@@ -27,6 +34,8 @@ public class Inicio extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         // validar
         if (getArguments() != null) {
@@ -39,17 +48,45 @@ public class Inicio extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // agregar binding
+        @NonNull FragmentInicioBinding binding = FragmentInicioBinding.inflate(inflater, container, false);
+        View view = binding.getRoot(); // obtener ruta
+        // acceder al boton
+        binding.button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // validar texto
+                if(!binding.editTextText.getText().toString().isEmpty()) {
+                    goPregunta(binding.editTextText.getText().toString());
+                } else {
+                    Toast.makeText(getContext(), "Campo vacio",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         // Inflate the layout for this fragment
         //vista = inflater.inflate(R.layout.fragment_inicio, container, false);
         // 5 referenciar btn usando como puente vista
 //        button = (Button) vista.findViewById(R.id.button);
 
-
-
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        //return inflater.inflate(R.layout.fragment_inicio, container, false);
+        return view;
     }
 
-    @Override
+    // ir a Pregunta
+    private void goPregunta(String name) {
+        // instanciar Pregunta
+        Pregunta pregunta = new Pregunta();
+        FragmentManager fragmentManager = getChildFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                // Transaction
+        FragmentTransaction transaction = fragmentManager.beginTransaction().replace(R.id.contenedorFragment,pregunta, Pregunta.class.getSimpleName());
+        transaction.commit();
+
+    }
+
+    /*@Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // referenciar btn frag
@@ -57,16 +94,16 @@ public class Inicio extends Fragment {
         Button button = view.findViewById(R.id.button);
 
         // 6 usar un Toast para probar evento btn ok
-        /*button.setOnClickListener(new View.OnClickListener() {
+        *//*button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(getContext(), "Presiona", Toast.LENGTH_LONG).show();
                 Navigation.findNavController(v).navigate(R.id.pregunta);
             }
-        });*/
+        });*//*
         button.setOnClickListener(v -> {
             Navigation.findNavController(v).navigate(R.id.pregunta);
         });
 
-    }
+    }*/
 }
